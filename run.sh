8056e833ffd79bc614201b63e75b11a346e6672c
Kai <tankaiwei@gmail.com>
2020-03-11 15:14:31 +0800

    standardise input

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 set -e
 
 echo "Getting weights from S3"
-aws s3 cp s3://kelong/weights/yolo-obj_best.weights backup/
+aws s3 cp s3://kelong/weights/yolo-obj_best.weights backup/yolo-obj_best.weights
+aws s3 cp s3://kelong/cfg/yolo-obj.cfg yolo-obj-test.cfg
 
 echo "running gunicorn..."
 gunicorn --bind 0.0.0.0:8888 wsgi:app
